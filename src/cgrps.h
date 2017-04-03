668976a3d2c8e9e491ea3b8e9970565e4812a57b
debian <eaosfu@users.noreply.github.com>
2017-04-03 12:23:48 -0700

    Missing cgrps.h in last commit

---
+++ src/cgrps.h
@@ -0,0 +1,22 @@
+#ifndef CGRPS_H_
+#define CGRPS_H_
+#include "bits.h"
+
+#define CGRP_MAX 9
+
+#define CGRP_MAP_SIZE                                            \
+  ((CGRP_MAX/BITS_PER_BLOCK) == 0) ? 1 : (CGRP_MAX/BITS_PER_BLOCK)
+
+#define mark_closure_map_backref(cgrp, i)                              \
+  ({do {                                                               \
+    if(((i) > 0) && ((i) <= CGRP_MAX)) {                               \
+      set_bit_array(BIT_MAP_TYPE, (cgrp), BITS_PER_BLOCK, (i));   \
+    }                                                                  \
+  } while(0);})
+
+#define cgrp_has_backref(cgrp, i)                                 \
+  ((((i) > 0) && ((i) <= CGRP_MAX))                                \
+  ? (check_bit_array(BIT_MAP_TYPE, (cgrp), BITS_PER_BLOCK, (i))) \
+  : 0)
+
+#endif
